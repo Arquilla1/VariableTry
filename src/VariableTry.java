9a8d3347aa53929c1195c992163cacda7fa6bb42
Arquilla1 <Arquilla1@msn.com>
2020-04-14 11:53:30 -0400

    first commit

---
+++ src/VariableTry.java
@@ -0,0 +1,15 @@
+public class VariableTry {
+
+    public static void main(String[] args) {
+
+        int TestScore = 85;
+        double priceValue =900.900;
+        String  username = "Karen Smith";
+
+
+        System.out.println("GPS: "+ TestScore );
+        System.out.println("Price: "+ priceValue);
+        System.out.println("User Name: "+ username);
+        
+    }
+}
